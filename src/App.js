b973cbd1107eee3b2a8aeafe4d546f5cae18b834
Duy Huynh Quoc <duyhq.zee@gmail.com>
2022-07-19 00:13:01 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Main from "./components/MainComponents";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./redux/configureStore";
+import TestFetchComponet from "./useFetch/TestFetchComponet";
 const store = ConfigureStore();
 class App extends Component {
-   render() {
-      return (
-         <Provider store={store}>
-            <BrowserRouter>
-               <div className="App">
-                  <Main />
-               </div>
-            </BrowserRouter>
-         </Provider>
-      );
-   }
+	render() {
+		return (
+			<Provider store={store}>
+				<BrowserRouter>
+					<div className="App">
+						{/* <Main /> */}
+						<TestFetchComponet />
+					</div>
+				</BrowserRouter>
+			</Provider>
+		);
+	}
 }
 
 export default App;
